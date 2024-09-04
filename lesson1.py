ecca6dcb1e982b8e04f788c94e42464794ba36a1
ChintanMalbari <chintanmalbari@gmail.com>
2024-09-04 23:35:12 +0000

    Lesson 1 Done

---
+++ lesson1.py
@@ -0,0 +1,15 @@
+# name = input("What is your name?").upper()
+# print("Hello " + name)
+# print(f"Hello, {name}.")
+
+
+# def greet(name):
+#     print(f"Hello, {name}.")
+# greet("Chintan")
+
+def add(a,b):
+    print(a + b)
+
+add(1,2)
+
+
